6fb936782a660937c23beb770e4324b8b5202aca
enessmr <enessmr22@gmail.com>
2025-07-16 19:22:14 +0300

    no

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -1,10 +1,10 @@
 import app from "ags/gtk4/app"
 import style from "./style.scss"
-import Bar from "./widget/Bar"
+import Bar from "./modules/bar"
 
 app.start({
   css: style,
   main() {
-    app.get_monitors().map(Bar)
+    app.get_monitors().map(monitor => new Bar(monitor))
   },
 })
