c47614e93711f223bef0848c2ed68780e9001014
enessmr <enessmr22@gmail.com>
2025-07-19 16:39:40 +0300

    Trashed buttonsOnDesktop. I quit on that. I am gonna stick with reminders and keybinds.

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -2,16 +2,17 @@ import Gtk from 'gi://Gtk?version=4.0'; // Needed to create windows
 import app from "ags/gtk4/app";
 import style from "./style.scss";
 import Bar from "./widget-tsx/Bar";
-import ButtonsOnDesktop from "./widget-tsx/ButtonsOnDesktop";
+import Reminder from "./widget-tsx/Reminder";
 import Gdk from 'gi://Gdk?version=4.0'; // Needed for monitor information
 import { parseApps } from "./hacks/parseApps";
+import Astal from 'gi://Astal?version=4.0'; // Needed for Astal functionalities
 
 app.start({
   css: style,
   main() {
     // Create bars per monitor (you probably do this already)
     app.get_monitors().forEach(monitor => new Bar(monitor));
-    app.get_monitors().forEach(monitor => new ButtonsOnDesktop(monitor));
+    // app.get_monitors().forEach(monitor => new Reminder(monitor));
     print("Loaded apps:", JSON.stringify(parseApps, null, 2));
   },
 });
