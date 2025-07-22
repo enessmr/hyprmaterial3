d574797715e9755bda021f5c1a4f544501a4398e
enessmr <enessmr22@gmail.com>
2025-07-22 16:39:02 +0300

    some s fix

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -5,12 +5,14 @@ import Bar from "./widget-tsx/Bar";
 import Gdk from 'gi://Gdk?version=4.0'; // Needed for monitor information
 import { parseApps } from "./hacks/parseApps";
 import Astal from 'gi://Astal?version=4.0'; // Needed for Astal functionalities
+import { subprocess, exec, execAsync, createSubprocess } from "ags/process"
 
 app.start({
   css: style,
   main() {
     // Create bars per monitor (you probably do this already)
     app.get_monitors().forEach(monitor => new Bar(monitor));
+    execAsync("matugen image ~/Pictures/.Wallpapers/wallpaper.jpg")
     print("Loaded apps:", JSON.stringify(parseApps, null, 2));
   },
 });
