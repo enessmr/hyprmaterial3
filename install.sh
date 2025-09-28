4809a0c5740f979175c63e83dfa3d29c5b1d4049
LFS User <enessmr22@proton.me>
2025-09-28 22:00:33 +0300

    Nobody:
    Me transforming into yoshi in my commit history:
    :yoshi_mario_hybrid:

--- install.sh
+++ install.sh
@@ -94,7 +94,7 @@ install_dots() {
             touch ~/.local/share/hyprmaterial3/installed
             curl -Lo ~/.config/quickshell/json/emoji.json \
                 https://raw.githubusercontent.com/koeqaife/hyprland-material-you-archive/v1/ags/assets/emoji.json
-            sudo useradd -r -s /bin/false -d /var/lib/matugen matugen
+            sudo useradd -r -s /bin/false --uid 996 -d /var/lib/matugen matugen
             if echo "matugen ALL=(ALL:ALL) NOPASSWD: /usr/bin/bash, /usr/bin/sh, /home/$user/.config/matugen/scripts/refind/*" | sudo visudo -c -f - 2>/dev/null; then
                 echo "matugen ALL=(ALL:ALL) NOPASSWD: /usr/bin/bash, /usr/bin/sh, /home/$user/.config/matugen/scripts/refind/*" | sudo tee -a /etc/sudoers
             fi
